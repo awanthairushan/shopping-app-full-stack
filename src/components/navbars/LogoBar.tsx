a4fb7a3c1b67fecabffd10abc1ad61ccb5b964a5
awanthairushan <2019is067@stu.ucsc.cmb.ac.lk>
2022-04-03 21:41:54 +0530

    created Promotion section UX

--- src/components/navbars/LogoBar.tsx
+++ src/components/navbars/LogoBar.tsx
@@ -6,7 +6,7 @@ import {ShoppingCart} from 'react-feather';
 const LogoBar: React.FC = () => {
     return (
         <Row className="logobar">
-            <Col xl={9} lg={8} sm={7} xs={6} className="ps-4">
+            <Col xl={9} lg={8} sm={7} xs={6} className="ps-4 ">
                 <Image src={Logo} className="img-fluid ms-md-5"/>
             </Col>
             <Col xl={3} lg={4} sm={5} xs={6}>
