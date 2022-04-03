e6b27d25a17788147f4d37bfd69f11071dc6a13c
awanthairushan <2019is067@stu.ucsc.cmb.ac.lk>
2022-04-03 18:43:34 +0530

    structured the home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.scss';
 import ECommerceApp from './view/ECommerceApp';
+import {Container} from 'react-bootstrap';
 
 function App() {
-  return (
-      <ECommerceApp/>
-  );
+    return (
+        <Container fluid className="p-0 m-0">
+            <ECommerceApp/>
+        </Container>
+    );
 }
 
 export default App;
--- src/view/ECommerceApp.tsx
+++ src/view/ECommerceApp.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Home from '../components/pages/Home';
-import {Container, Row, Col} from 'react-bootstrap';
+import {Row, Col} from 'react-bootstrap';
 
 const ECommerceApp: React.FC = () => {
     return (
-            <Row xs={12}>
+        <Row>
+            <Col>
                 <Home/>
-            </Row>
+            </Col>
+        </Row>
     )
 }
 
