aa5ff4d47911a5bee69fc98978f5e4a373d9dfeb
Mihindu Wijesena <mihinduwijesena@gmail.com>
2022-04-06 08:48:16 +0530

    Mouse hover for login and register

--- src/components/navbars/ContactBar.tsx
+++ src/components/navbars/ContactBar.tsx
@@ -5,26 +5,30 @@ import {PhoneCall, Truck} from "react-feather";
 const ContactBar: React.FC = () => {
   return (
     <Row>
-      <Col xs={12} md={6} className="offset-6 contact-bar">
+      <Col xs={12} md={6} className="offset-md-6 offset-sm-0 contact-bar">
         <Row>
-          <Col xs={12} md={3} className="offset-3 p-0 contact-details">
-            <Truck className="me-1 text-end offset-2"/>
+          <Col xs={12} md={3} className="offset-md-3 offset-sm-0 p-0 contact-details">
+            <Truck className="me-1 text-end offset-md-2"/>
             <span className="pe-3 mt-2 text-end ">Delivery Areas</span>
           </Col>
           <Col xs={12} md={3} className="contact-details">
-            <PhoneCall className="m-1 offset-3" />
+            <PhoneCall className="m-1 offset-md-3 offset-sm-0 p-0" />
             <span className="pe-3">+94 112 123456</span>
           </Col>
-          <Col xs={12} md={3} className="m-0 p-0  label-color">
-            <span className="pe-3">Register</span>
-            <span>Login</span>
+          <Col xs={12} md={3} className="m-0 p-0 ">
+            <Row>
+              <Col xs={6} className="label-color">
+                <span className="pe-3 contact-details ">Register</span>
+              </Col>
+              <Col xs={6} className="label-color">
+                <span className="contact-details">Login</span>
+              </Col>
+            </Row>
           </Col>
         </Row>
       </Col>
     </Row>
-
   );
-
 }
 
 export default ContactBar;
