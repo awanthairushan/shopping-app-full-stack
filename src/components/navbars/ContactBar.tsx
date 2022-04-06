8acef216111d20f268ba16954a6bec4f960e6e69
Mihindu Wijesena <mihinduwijesena@gmail.com>
2022-04-06 07:32:02 +0530

    Contact bar colors and hover over icon fix

--- src/components/navbars/ContactBar.tsx
+++ src/components/navbars/ContactBar.tsx
@@ -5,17 +5,17 @@ import {PhoneCall, Truck} from "react-feather";
 const ContactBar: React.FC = () => {
   return (
     <Row>
-      <Col xs={12} md={6} className="offset-6">
+      <Col xs={12} md={6} className="offset-6 contact-bar">
         <Row>
-          <Col xs={12} md={3} className="offset-3 p-0">
+          <Col xs={12} md={3} className="offset-3 p-0 contact-details">
             <Truck className="me-1 text-end offset-2"/>
-            <span className="pe-3 text-end">Delivery Areas</span>
+            <span className="pe-3 mt-2 text-end ">Delivery Areas</span>
           </Col>
-          <Col xs={12} md={3}>
+          <Col xs={12} md={3} className="contact-details">
             <PhoneCall className="m-1 offset-3" />
             <span className="pe-3">+94 112 123456</span>
           </Col>
-          <Col xs={12} md={3} className="m-0 p-0 ">
+          <Col xs={12} md={3} className="m-0 p-0  label-color">
             <span className="pe-3">Register</span>
             <span>Login</span>
           </Col>
