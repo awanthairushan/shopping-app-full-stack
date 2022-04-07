9ce283a0e784b3c3e8fb877e47af0cb9ee8f0d9b
A.M.V.N.Attanayaka <73266263+VimukthiniAttanayaka@users.noreply.github.com>
2022-04-07 09:40:54 +0530

    logobar sum number veriable add

--- src/components/navbars/LogoBar.tsx
+++ src/components/navbars/LogoBar.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import {Row, Col, Image} from 'react-bootstrap';
+import { Row, Col, Image } from 'react-bootstrap';
 import Logo from '../../assets/images/LOGO.png';
-import {ShoppingCart} from 'react-feather';
+import { ShoppingCart } from 'react-feather';
 
 const LogoBar: React.FC = () => {
     return (
         <Row className="logobar">
             <Col xl={9} lg={8} sm={7} xs={6} className="ps-4 ">
-                <Image src={Logo} className="img-fluid ms-md-5"/>
+                <Image src={Logo} className="img-fluid ms-md-5" />
             </Col>
-            <Col xl={3} lg={4} sm={5} xs={6}>
-                <ShoppingCart className="shopping-cart mt-md-2 me-2"/>
-                <button type="button" className=" py-0 btn text-light ms-md-4 ">Check out</button>
+            <Col xl={1} lg={1} sm={1} xs={1}>
+                <ShoppingCart className="shopping-cart mt-md-2 me-2" />
+                <span className="d-flex align-items-center justify-content-center cart-basket">
+                    0
+                </span>
+                </Col>
+                <Col xl={2} lg={3} sm={4} xs={5}>
+                <button type="button" className=" py-0 btn text-light">Check out</button>
             </Col>
         </Row>
     )
