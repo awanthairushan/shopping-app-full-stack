ec8b9418d3dd0d726d2ed30bc33394e9077209e6
Mihindu Wijesena <mihinduwijesena@gmail.com>
2022-04-08 22:19:16 +0530

    Change to NavBar

--- src/components/navbars/ContactBar.tsx
+++ src/components/navbars/ContactBar.tsx
@@ -1,38 +1,26 @@
 import React from 'react';
-import {Col, Row} from "react-bootstrap";
+import {Nav} from "react-bootstrap";
 import {PhoneCall, Truck} from "react-feather";
 
 const ContactBar: React.FC = () => {
   return (
-    <Row >
-      <Col md={{span: 6, offset: 6}} className="contact-bar">
-        <Row>
-          <Col xs={12} md={3} className="offset-md-2 d-inline-flex align-self-end
-            offset-sm-0 d-flex justify-content-center">
-            <span className="label text-muted ">
-              <Truck/>
-              <span className="ps-2 ">Delivery Areas</span>
-            </span>
-          </Col>
-          <Col xs={12} md={3} className="d-inline-flex align-items-end d-flex justify-content-center">
-            <span className="label" >
-              <PhoneCall/>
-              <span className="ps-2 text-black">+94 112 123456</span>
-            </span>
-          </Col>
-          <Col xs={12} md={2} className="d-inline-flex align-items-end pe-0 d-flex justify-content-center buttons">
-            <label className="buttons">
-              Login
-            </label>
-          </Col>
-          <Col xs={12} md={2} className="d-inline-flex align-items-end ms-0 d-flex justify-content-center buttons">
-            <label>
-              Register
-            </label>
-          </Col>
-        </Row>
-      </Col>
-    </Row>
+    <Nav className="justify-content-end contact-bar ">
+      <Nav.Item className="text-muted label">
+        <Truck />
+        <span className="ms-2">Delivery Areas</span>
+      </Nav.Item>
+      <Nav.Item className="label ms-4">
+        <PhoneCall />
+        <span className="ms-2">+94 112 123456</span>
+      </Nav.Item>
+      <Nav.Item className="buttons ms-4">
+        <span className="ms-2" >Register</span>
+      </Nav.Item>
+      <Nav.Item className="label ps-4">
+        <span>Login</span>
+      </Nav.Item>
+    </Nav>
+
   );
 }
 
