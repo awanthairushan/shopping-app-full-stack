506f86857139907665e9cec85817151c301b9588
A.M.V.N.Attanayaka <73266263+VimukthiniAttanayaka@users.noreply.github.com>
2022-04-24 23:39:57 +0530

    cart font size change

--- src/components/pages/CartDropDown.tsx
+++ src/components/pages/CartDropDown.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Row, Col, Image } from 'react-bootstrap';
+import { Row, Col, Image, Button } from 'react-bootstrap';
 import 'react-dropdown/style.css';
 import { ICart } from '../../Types/ShoppingTypes';
 import CartDropDownItem from './CartDropDownItem';
-import cartempty from './../../assets/images/cart-empty.jpg';
-import {Triangle} from "react-feather";
+import cartempty from './../../assets/images/cart.png';
 
 type CartDropDownProp = {
     cartItems: ICart[],
@@ -42,15 +41,17 @@ const CartDropDown: React.FC<CartDropDownProp> = (props) => {
     if (cartItems.length === 0) {
         return (
             <Row className='cart-priview-header'>
-                <Col xs={12}>
-                <Image src={cartempty} className="cart-empty"/>
+                <Col xs={12} className='p-0'>
+                    <Image src={cartempty} className="cart-empty" />
+                    <p className='cart-empty-text colour-red font-12px'>Your Cart is empty</p>
+                    <p className='cart-empty-text colour-gray font-12px'>Add items to your cart :)</p>
                 </Col>
             </Row>
         );
     }
     const renderCartItems = () => {
         return (
-            <>
+            <Row className='cart-items'>
                 {cartItems.map((item: ICart, index: number) => (
                     <CartDropDownItem
                         item={item}
@@ -58,39 +59,40 @@ const CartDropDown: React.FC<CartDropDownProp> = (props) => {
                         key={index}
                     />
                 ))}
-            </>
+            </Row>
         );
     };
 
     return (
         <Row className='cart-priview-header'>
-            <Col xs={12}>
+            <Col xs={12} className='cart-content'>
                 {/* <Triangle className='triangle'/> */}
                 {renderCartItems()}
-                <Row>
-                    <Col xs={7}>
+                <Row className='mt-3'>
+                    <Col xs={8}>
                         <Row>
-                            <h5>Subtotal ({totalItemSet()} items)</h5>
+                            <h5 className='font-12px ps-0'>Subtotal ({totalItemSet()} items)</h5>
                         </Row>
                         <Row>
-                            <h5>Discount</h5>
+                            <h5 className='font-12px ps-0'>Discount</h5>
                         </Row>
                     </Col>
-                    <Col xs={5} className="cart-values">
-                        <h5 className='cart-subtotal'>Rs. {subTotalSet()}.00</h5>
-                        <h5>Rs. {discountSet()}.00</h5>
+                    <Col xs={4} className="cart-values">
+                        <h5 className='colour-red font-12px pe-4'>Rs. {subTotalSet()}.00</h5>
+                        <h5 className='font-12px pe-4'>Rs. {discountSet()}.00</h5>
                     </Col>
-                    <hr />
+                    <hr className='hr' />
                 </Row>
 
                 <Row>
-                    <Col xs={6}>
-                        <h5>Total</h5>
+                    <Col xs={6} className='ps-0'>
+                        <h5 className='font-12px ps-0'>Total</h5>
                     </Col>
                     <Col xs={6} className="cart-values">
-                        <h5 className='cart-total'>Rs. {totalSet()}.00</h5>
+                        <h5 className='colour-red font-12px pe-4'>Rs. {totalSet()}.00</h5>
                     </Col>
                 </Row>
+                <Button className='cart-checkout mb-3 mt-1'>checkout</Button>
             </Col>
         </Row>
     )
