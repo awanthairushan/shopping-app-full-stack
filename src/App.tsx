20b5090b71dcb6c40a124a59d0075243e7b28468
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-04-25 18:08:16 +0530

    Refactored the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React, {useState} from 'react';
 import './App.scss';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import ECommerceApp from './view/ECommerceApp';
@@ -6,35 +6,35 @@ import Home from "./components/pages/Home";
 import AboutUs from "./components/pages/staticpages/AboutUs";
 import ContactUs from "./components/pages/staticpages/ContactUs";
 import FAQ from "./components/pages/staticpages/FAQ";
-import { ICart } from './Types/ShoppingTypes';
-import { useToasts } from 'react-toast-notifications';
+import {ICart} from './Types/ShoppingTypes';
+import {useToasts} from 'react-toast-notifications';
 import CheckOut from "./components/pages/CheckOut";
 import Pricing from "./components/pages/staticpages/Pricing";
 
 const App = () => {
-    const { addToast } = useToasts();
+    const {addToast} = useToasts();
     const CartList: ICart[] = [];
     const [cartItems, setCartItems] = useState<ICart[]>(CartList);
 
     const handleOnCartItemCreate = (newItem: ICart) => {
         const allItems: ICart[] = cartItems.slice();
-        var num=0;
-        for(var i=0; i<allItems.length;i++){
-            if(allItems[i].name === newItem.name){
+        var num = 0;
+        for (var i = 0; i < allItems.length; i++) {
+            if (allItems[i].name === newItem.name) {
                 allItems.splice(i, 1, newItem);
                 setCartItems(allItems);
-                num=1;
+                num = 1;
                 return;
             } else {
-                num=0;
+                num = 0;
             }
         }
-        if(num===0){
-        allItems.push(newItem);
-        setCartItems(allItems);
+        if (num === 0) {
+            allItems.push(newItem);
+            setCartItems(allItems);
         }
-        addToast("Item Add To Cart", { appearance: 'success', autoDismiss: true });
-      };
+        addToast("Item Add To Cart", {appearance: 'success', autoDismiss: true});
+    };
     return (
         <BrowserRouter>
             <Routes>
