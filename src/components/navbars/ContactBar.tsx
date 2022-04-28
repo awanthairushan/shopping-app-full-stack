aa92230d87d81d3dabca36ae82530d2bdbf30993
Mihindu Wijesena <mihinduwijesena@gmail.com>
2022-04-28 18:31:02 +0530

    remove cusor point for not working items add anchor to telephone details add color to Login label

--- src/components/navbars/ContactBar.tsx
+++ src/components/navbars/ContactBar.tsx
@@ -4,19 +4,20 @@ import {PhoneCall, Truck} from "react-feather";
 
 const ContactBar: React.FC = () => {
   return (
-    <Nav className="justify-content-md-end justify-content-center contact-bar my-2">
-      <Nav.Item className="text-muted label">
+    <Nav className="justify-content-md-end justify-content-center contact-bar my-2 pe-4 me-xl-4 ps-xl-3 ps-sm-3 ps-0">
+      <Nav.Item className="text-muted label ps-2">
         <Truck />
         <span className="ms-2">Delivery Areas</span>
       </Nav.Item>
-      <Nav.Item className="ms-4">
-        <PhoneCall />
-        <span className="ms-2">+94 112 123456</span>
+      <Nav.Item>
+          <span className="mx-2">
+            <a href="tel:+94 112 123456" className="href ms-2 fw-bold"> <PhoneCall /> +94 112 123456</a>
+          </span>
       </Nav.Item>
-      <Nav.Item className="buttons ms-4">
-        <span className="px-3" >Register</span>
+      <Nav.Item className="buttons ms-2">
+        <span className="px-2 mx-2" >Register</span>
       </Nav.Item>
-      <Nav.Item className="label ps-4">
+      <Nav.Item className="label ps-2 ms-3 me-2">
         <span>Login</span>
       </Nav.Item>
     </Nav>
