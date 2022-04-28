4a759493ef82535cc2cea5e5445429e84511c2a9
Mihindu Wijesena <mihinduwijesena@gmail.com>
2022-04-28 19:55:53 +0530

    add cusor point and add two classes for login and register buttons

--- src/components/navbars/ContactBar.tsx
+++ src/components/navbars/ContactBar.tsx
@@ -6,18 +6,18 @@ const ContactBar: React.FC = () => {
   return (
     <Nav className="justify-content-md-end justify-content-center contact-bar my-2 pe-4 me-xl-4 ps-xl-3 ps-sm-3 ps-0">
       <Nav.Item className="text-muted label ps-2">
-        <Truck />
+        <Truck/>
         <span className="ms-2">Delivery Areas</span>
       </Nav.Item>
       <Nav.Item>
           <span className="mx-2">
-            <a href="tel:+94 112 123456" className="href ms-2 fw-bold"> <PhoneCall /> +94 112 123456</a>
+            <a href="tel:+94 112 123456" className="href ms-2"> <PhoneCall/> +94 112 123456</a>
           </span>
       </Nav.Item>
-      <Nav.Item className="buttons ms-2">
-        <span className="px-2 mx-2" >Register</span>
+      <Nav.Item className="register-button ms-2">
+        <span className="px-2 mx-2">Register</span>
       </Nav.Item>
-      <Nav.Item className="label ps-2 ms-3 me-2">
+      <Nav.Item className="button-login ps-2 ms-3 me-2">
         <span>Login</span>
       </Nav.Item>
     </Nav>
