549e89251683a9758ced7b0e2539dfb08a8ee1c0
awanthairushan <2019is067@stu.ucsc.cmb.ac.lk>
2022-04-29 11:32:07 +0530

    Refactored the code

--- src/components/navbars/LogoBar.tsx
+++ src/components/navbars/LogoBar.tsx
@@ -14,7 +14,7 @@ const LogoBar: React.FC<LogoBarProp> = (props) => {
     const [isCartVisible, setIsCartVisible] = useState(false);
 
     const cartVisible = () => {
-        if (isCartVisible === true) {
+        if (isCartVisible) {
             setIsCartVisible(false)
         } else {
             setIsCartVisible(true);
@@ -22,11 +22,12 @@ const LogoBar: React.FC<LogoBarProp> = (props) => {
     }
 
     return (
-        <Row className="logobar">
+        <Row className="logo-bar">
             <Col lg={9} sm={8} xs={10} className="ps-3 ps-sm-4">
                 <Image src={Logo} className="img-fluid ms-md-5 ms-sm-4"/>
             </Col>
-            <Col lg={1} sm={1} xs={2} onClick={cartVisible} className='ms-xl-3 ms-lg-0 shopping-cart-and-basket ps-md-0 ps-sm-1 ps-0'>
+            <Col lg={1} sm={1} xs={2} onClick={cartVisible}
+                 className='ms-xl-3 ms-lg-0 shopping-cart-and-basket ps-md-0 ps-sm-1 ps-0'>
                 <ShoppingCart className="shopping-cart mt-lg-0 me-0"/>
                 <span className="d-flex align-items-center justify-content-center cart-basket">
                     {cartItems.length}
