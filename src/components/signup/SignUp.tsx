551cde3c75285e804b4c4b67b7bd1f6ada7f00b1
awanthairushan <2019is067@stu.ucsc.cmb.ac.lk>
2022-05-31 12:22:26 +0530

    added icons and validation

--- src/components/signup/SignUp.tsx
+++ src/components/signup/SignUp.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import {Button, Form} from "react-bootstrap";
-import {User} from 'react-feather'
+import {Button, Form, InputGroup} from "react-bootstrap";
+import {User, Mail, Phone, Lock} from 'react-feather'
 
 const SignUp = () => {
     return (
@@ -10,21 +10,28 @@ const SignUp = () => {
                 <Form.Text className="text-muted">
                     We'll never share your details with anyone else.
                 </Form.Text>
-                <Form.Group className="my-3 data-field" controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Email"/>
-                </Form.Group>
-                <Form.Group className="mb-3 data-field" controlId="formBasicName">
-                    <Form.Control type="text" placeholder="Name"/>
-                </Form.Group>
-                <Form.Group className="mb-3 data-field" controlId="formBasicContact">
-                    <Form.Control type="text" placeholder="Contact No"/>
-                </Form.Group>
-                <Form.Group className="mb-3 data-field" controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="Password"/>
-                </Form.Group>
-                <Form.Group className="mb-3 data-field" controlId="formBasicRePassword">
-                    <Form.Control type="password" placeholder="Repassword"/>
-                </Form.Group>
+                <InputGroup className="my-3 data-field" id="formBasicEmail">
+                    <InputGroup.Text className='data-field-icon' id="basic-addon1"><Mail/></InputGroup.Text>
+                    <Form.Control type="email" placeholder="Email" required/>
+                </InputGroup>
+                <InputGroup className="mb-3 data-field" id="formBasicName">
+                    <InputGroup.Text className='data-field-icon' id="basic-addon1"><User/></InputGroup.Text>
+                    <Form.Control type="text" placeholder="Name" required pattern="[A-Za-z]+"/>
+                </InputGroup>
+                <InputGroup className="mb-3 data-field" id="formBasicContact">
+                    <InputGroup.Text className='data-field-icon' id="basic-addon1"><Phone/></InputGroup.Text>
+                    <Form.Control type="text" placeholder="Contact No" required pattern='^[0][\d]{9}$'/>
+                </InputGroup>
+                <InputGroup className="mb-3 data-field" id="formBasicPassword">
+                    <InputGroup.Text className='data-field-icon' id="basic-addon1"><Lock/></InputGroup.Text>
+                    <Form.Control type="password" placeholder="Password" required
+                                  pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"/> {/* Minimum eight characters, at least one letter and one number:*/}
+                </InputGroup>
+                <InputGroup className="mb-3 data-field" id="formBasicRePassword">
+                    <InputGroup.Text className='data-field-icon' id="basic-addon1"><Lock/></InputGroup.Text>
+                    <Form.Control type="password" placeholder="Repassword" required
+                                  pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"/> {/* Minimum eight characters, at least one letter and one number:*/}
+                </InputGroup>
                 <Button className='signup-button' type="submit">
                     SIGN UP
                 </Button>
