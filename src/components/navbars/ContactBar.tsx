e74fd6c315b8bb52246e0c568e12130bbddf0c4f
Mihindu Wijesena <mihinduwijesena@gmail.com>
2022-06-02 20:34:16 +0530

    Change color of login button and apply css href, background color removed from nav bar

--- src/components/navbars/ContactBar.tsx
+++ src/components/navbars/ContactBar.tsx
@@ -7,7 +7,7 @@ const ContactBar: React.FC = () => {
   return (
     <Row>
       <Col xs={12} className="ms-sm-0 ps-sm-0 ps-3 me-xl-4">
-        <Navbar bg="light" expand="lg" className="contact-bar float-end">
+        <Navbar expand="lg" className="contact-bar float-end">
           <Container>
             <Navbar.Brand href="#home">
               <span className="mx-2">
@@ -25,7 +25,7 @@ const ContactBar: React.FC = () => {
                   <span className="px-2 mx-2">Register</span>
                 </Nav.Item>
                 <Nav.Item className="button-login ps-2 ms-3 me-2 d-flex float-end">
-                  <span><Link to={"LoginAndSignup"}>Login</Link></span>
+                  <span><Link to={"LoginAndSignup"} className="href">Login</Link></span>
                 </Nav.Item>
               </Nav>
             </Navbar.Collapse>
