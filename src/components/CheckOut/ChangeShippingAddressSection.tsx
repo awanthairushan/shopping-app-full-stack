87bf02df499fda20e731f315af16ab45fac4a0c6
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-06-28 16:16:07 +0530

    Created the UI for payment type

--- src/components/CheckOut/ChangeShippingAddressSection.tsx
+++ src/components/CheckOut/ChangeShippingAddressSection.tsx
@@ -1,6 +1,9 @@
 import React, {ChangeEvent, FC, useState} from "react";
-import {Col, Form, FormCheck, FormControl, FormGroup, FormLabel, Image} from "react-bootstrap";
+import {Col, Form, FormCheck, FormControl, FormGroup, FormLabel, Image, Row} from "react-bootstrap";
 import {FaRegCreditCard, FaRegMoneyBillAlt} from "react-icons/fa";
+
+import cash from '../../assets/images/CheckOutForm/cash.png';
+import card from '../../assets/images/CheckOutForm/creditCard.png';
 
 const ChangeShippingAddressSection: FC = () => {
 
@@ -17,6 +20,9 @@ const ChangeShippingAddressSection: FC = () => {
         }
     }
 
+    const handleOnPaymentTypeSelect = (e: ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.id)
+    }
     return (
         <Col className="px-0 mt-lg-4">
             <span>Change Shipping Address</span>
@@ -34,14 +40,23 @@ const ChangeShippingAddressSection: FC = () => {
                 <FormLabel className='my-lg-2'>Payment Methods</FormLabel>
 
                 <FormGroup className='payment-method'>
-                    <FormLabel>
-                        <FaRegCreditCard id='abc' className='payment-method-icon'/>
-                        <FormCheck name='paymentMethod' type='radio' label='Credit/Debit Card'/>
-                    </FormLabel>
-                    <FormLabel>
-                        <FaRegMoneyBillAlt className='payment-method-icon'/>
-                        <FormCheck name='paymentMethod' type='radio' label='Cash on Delivery'/>
-                    </FormLabel>
+                    <Row className='mx-3' >
+                        <Col className='payment-method-card px-0 me-2' id='card' onChange={handleOnPaymentTypeSelect}>
+                            <FormLabel className='d-grid justify-content-center m-0'>
+                                <input type='radio' name='payment-method' value='cash'/>
+                                <FaRegCreditCard fontSize='100px' display='block'/>
+                                <p>Credit/Debit Card</p>
+                            </FormLabel>
+                        </Col>
+                        <Col className='payment-method-card px-0 ms-3' id='cash' onChange={handleOnPaymentTypeSelect}>
+                            <FormLabel className='d-grid justify-content-center'>
+                                <input type='radio' name='payment-method' value='card'/>
+                                <FaRegMoneyBillAlt fontSize='100px'/>
+                                <p>Cash on Delivery</p>
+                            </FormLabel>
+                        </Col>
+                    </Row>
+
                 </FormGroup>
             </Form>
         </Col>
