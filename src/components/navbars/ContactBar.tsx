74c7a3d49706144ddd7b666cd7ade51c39c9c3ce
A.M.V.N.Attanayaka <73266263+VimukthiniAttanayaka@users.noreply.github.com>
2022-07-06 22:48:13 +0530

    make contact number alignment

--- src/components/navbars/ContactBar.tsx
+++ src/components/navbars/ContactBar.tsx
@@ -1,35 +1,42 @@
-import React from 'react';
-import {Col, Container, Nav, Navbar, Row} from "react-bootstrap";
-import {PhoneCall, Truck, User} from "react-feather";
-import {Link} from "react-router-dom";
+import React, { useState } from 'react';
+import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
+import { PhoneCall, Truck, User } from "react-feather";
+import { Link } from "react-router-dom";
 
 const ContactBar: React.FC = () => {
+  const [show, setShow] = useState(true);
+  const collapse = () => {
+    if(show){
+      setShow(false);
+    } else {
+      setShow(true)
+    }
+  }
   return (
     <Row>
       <Col xs={12} className="ms-sm-0 ps-sm-0 ps-3 me-xl-4">
-        <Navbar expand="lg" className="contact-bar float-end">
-          <Container>
-            <Navbar.Brand href="#home">
-              <span className="mx-2">
-            <a href="tel:+94 112 123456" className="href ms-0">
-              <PhoneCall/> +94 112 123456</a>
-          </span></Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar expand="lg" className="contact-bar float-end py-lg-0">
+          <Container className='pe-lg-0'>
+            <Navbar.Brand href="#home" className='py-1'>
+                <a href="tel:+94 112 123456" className={show?"href":"href contact-number"}>
+                  <PhoneCall className='me-2' /> +94 112 123456</a>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={collapse} />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Item className="label ps-2 delivery-area ">
-                  <Truck className="truck"/>
-                  <span className="ms-2 ">Delivery Areas</span>
+                <Nav.Item className="label ps-lg-2 mt-2 mt-md-0 delivery-area ">
+                  <Truck className="truck" />
+                  <span className="ms-2 colour-gray ">Delivery Areas</span>
                 </Nav.Item>
-                <Nav.Item className="mx-3  my-account">
-                  <User/>
-                  <span className="ms-1">My Account</span>
+                <Nav.Item className="mx-lg-3 mt-2 mt-md-0 my-account">
+                  <User />
+                  <span className="ms-1 colour-gray">My Account</span>
                 </Nav.Item>
-                <Nav.Item className="register-button ms-2 d-flex float-end">
+                <Nav.Item className="register-button ms-lg-2 d-flex float-end mt-2 mt-md-0">
                   <span className="px-2 mx-2">Register</span>
                 </Nav.Item>
-                <Nav.Item className="button-login ps-2 ms-3 me-2 d-flex float-end">
-                  <span><Link to={"LoginAndSignup"} className="href">Login</Link></span>
+                <Nav.Item className="button-login ps-lg-2 ms-lg-3 me-2 d-flex float-end mt-2 mt-md-0 ">
+                  <span><Link to={"LoginAndSignup"} className="href login">Login</Link></span>
                 </Nav.Item>
               </Nav>
             </Navbar.Collapse>
