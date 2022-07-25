9e787f0aba9331c3dc2e35eedaa07d944438e1ae
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-07-25 22:20:29 +0530

    Completed customers section structure

---
+++ src/components/Admin/Customers/CustomerListItem.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from "react";
+import {Col} from "react-bootstrap";
+
+const CustomerListItem: FC = () => {
+    return (
+        <Col lg={10}>
+            CustomerListItem
+        </Col>
+    );
+}
+
+export default CustomerListItem;
--- src/components/Admin/Customers/Customers.tsx
+++ src/components/Admin/Customers/Customers.tsx
@@ -1,10 +1,14 @@
 import React, {FC} from "react";
 import {Col, Row} from "react-bootstrap";
+import CustomersList from "./CustomersList";
 
 const Customers: FC = () => {
     return (
-        <Row lg={10} className='content-wrapper'>
-            Customers
+        <Row className='content-wrapper d-grid p-lg-4 mx-0 br'>
+            <Row className='mx-0'>
+                Customers
+            </Row>
+            <CustomersList/>
         </Row>
     );
 }
