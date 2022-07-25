0961911f9d0db91da86f2d87bfde6e27a0fa4469
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-07-25 20:59:17 +0530

    Created the navigation bar sketch with routing

--- src/components/Admin/Navigation/LeftNavigation.tsx
+++ src/components/Admin/Navigation/LeftNavigation.tsx
@@ -1,10 +1,28 @@
 import React, {FC} from "react";
 import {Col} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 const LeftNavigation: FC = () => {
     return (
         <Col lg={2} className='left-navigation'>
-            Left Navigation
+            <div>Logo</div>
+            <ul>
+                <li>
+                    <Link to='/dashboard'>Dashboard</Link>
+                </li>
+                <li>
+                    <Link to='/products'>Products</Link>
+                </li>
+                <li>
+                    <Link to='/customers'>Customers</Link>
+                </li>
+                <li>
+                    <Link to='/orders'>Orders</Link>
+                </li>
+                <li>
+                    <Link to='/discounts'>Discounts</Link>
+                </li>
+            </ul>
         </Col>
     );
 }
