c9f22a8facda6cf8e568924dba77ae309c39fa1d
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-07-26 15:35:25 +0530

    Completed Left Navigation UI/UX

--- src/components/Admin/Navigation/LeftNavigation.tsx
+++ src/components/Admin/Navigation/LeftNavigation.tsx
@@ -1,27 +1,55 @@
-import React, {FC} from "react";
-import {Col} from "react-bootstrap";
-import {Link} from "react-router-dom";
+import React, {FC, useEffect, useState} from "react";
+import {Col, Image} from "react-bootstrap";
+import {Link, useLocation} from "react-router-dom";
+import Logo from "../../../assets/images/LOGO.png";
+import {FiHome, FiPercent, FiShoppingCart, FiTag, FiUser, FiUsers} from "react-icons/fi";
 
 const LeftNavigation: FC = () => {
+
+    const location = useLocation();
+    const [url, setURL] = useState<string>('');
+
+    useEffect(() => {
+        setURL(location.pathname);
+    }, [location]);
+
+
     return (
-        <Col lg={2} className='left-navigation'>
-            <div>Logo</div>
-            <ul>
-                <li>
-                    <Link to='/dashboard'>Dashboard</Link>
-                </li>
-                <li>
-                    <Link to='/products'>Products</Link>
-                </li>
-                <li>
-                    <Link to='/customers'>Customers</Link>
-                </li>
-                <li>
-                    <Link to='/orders'>Orders</Link>
-                </li>
-                <li>
-                    <Link to='/discounts'>Discounts</Link>
-                </li>
+        <Col lg={2} className='left-navigation px-0'>
+            <div className='logo-text px-lg-4 py-lg-4 fs-2'>
+                LOGO
+            </div>
+            <ul className='list-unstyled px-0 nav-list'>
+                <Link to='/dashboard' className={url === '/dashboard' ? 'active' : ''}>
+                    <li className='px-lg-4 '>
+                        <FiHome/>
+                        Dashboard
+                    </li>
+                </Link>
+                <Link to='/products' className={url === '/products' ? 'active' : ''}>
+                    <li className='px-lg-4'>
+                        <FiTag/>
+                        Products
+                    </li>
+                </Link>
+                <Link to='/customers' className={url === '/customers' ? 'active' : ''}>
+                    <li className='px-lg-4'>
+                        <FiUsers/>
+                        Customers
+                    </li>
+                </Link>
+                <Link to='/orders' className={url === '/orders' ? 'active' : ''}>
+                    <li className='px-lg-4'>
+                        <FiShoppingCart/>
+                        Orders
+                    </li>
+                </Link>
+                <Link to='/discounts' className={url === '/discounts' ? 'active' : ''}>
+                    <li className='px-lg-4'>
+                        <FiPercent/>
+                        Discounts
+                    </li>
+                </Link>
             </ul>
         </Col>
     );
