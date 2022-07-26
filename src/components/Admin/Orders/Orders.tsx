2903e41da91b5944ba04a6ad54d40f719effbac3
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-07-26 14:18:48 +0530

    Completed orders section structure

--- src/components/Admin/Orders/Orders.tsx
+++ src/components/Admin/Orders/Orders.tsx
@@ -1,10 +1,14 @@
 import React, {FC} from "react";
 import {Col, Row} from "react-bootstrap";
+import OrdersList from "./OrdersList";
 
 const Orders: FC = () => {
     return (
-        <Row lg={10} className='content-wrapper'>
-            Orders
+        <Row className='content-wrapper d-grid p-lg-4 mx-0 br'>
+            <Row className='mx-0'>
+                Orders
+            </Row>
+            <OrdersList/>
         </Row>
     );
 }
---
+++ src/components/Admin/Orders/OrdersList.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from "react";
+import {Col, Row} from "react-bootstrap";
+
+const OrdersList: FC = () => {
+    return (
+        <Row className='mx-0 p-lg-4 content' >
+            Orders list
+        </Row>
+    );
+}
+
+export default OrdersList;
