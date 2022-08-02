590bffd308fb3c361eee0ef80471d0e66b841ea7
A.M.V.N.Attanayaka <73266263+VimukthiniAttanayaka@users.noreply.github.com>
2022-08-02 22:06:34 +0530

    product table add

--- src/components/Admin/Products/Products.tsx
+++ src/components/Admin/Products/Products.tsx
@@ -1,14 +1,21 @@
-import React, {FC} from "react";
-import {Col, Row} from "react-bootstrap";
+import React, { FC } from "react";
+import { Button, Col, Row } from "react-bootstrap";
+import { Plus } from "react-feather";
 import ProductsList from "./ProductsList";
 
 const Products: FC = () => {
     return (
         <Row className='content-wrapper d-grid p-lg-4 mx-0'>
-            <Row className='mx-0'>
-                Products
+            <Row className='mx-0 p-0 pb-lg-4'>
+                <Col className="admin-product">
+                    <p className="m-0 fs-3">Products</p>
+                </Col>
+                <Col className="d-flex justify-content-end p-0">
+                    <Button className="add-product fs-6"> <Plus/> Add Products</Button>
+                </Col>
+
             </Row>
-            <ProductsList/>
+            <ProductsList />
         </Row>
     );
 }
--- src/components/Admin/Products/ProductsList.tsx
+++ src/components/Admin/Products/ProductsList.tsx
@@ -1,10 +1,43 @@
-import React, {FC} from "react";
-import {Col, Row} from "react-bootstrap";
+import React, { FC } from "react";
+import { Col, Row, Table } from "react-bootstrap";
 
 const ProductsList: FC = () => {
     return (
         <Row className='mx-0 p-lg-4 content' >
-            Product list
+            <Col>
+                <p className="fs-5 bold">Product list</p>
+                <Table bordered hover>
+                    <thead>
+                        <tr>
+                            <th className="bold p-3">Product Name</th>
+                            <th className="bold p-3">Category</th>
+                            <th className="bold p-3">Price</th>
+                            <th className="bold p-3">Quantity</th>
+                            <th className="bold p-3">Inventory</th>
+                            <th className="bold p-3">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+
+                    </tbody>
+                </Table>
+            </Col>
         </Row>
     );
 }
