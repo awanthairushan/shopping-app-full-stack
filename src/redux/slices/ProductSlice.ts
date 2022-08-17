4b1c0d73fc3b68d8155b3ecfde080eafabdd6532
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-08-17 19:27:29 +0530

    Added all reducers to productSlice

--- src/redux/slices/ProductSlice.ts
+++ src/redux/slices/ProductSlice.ts
@@ -1,1 +1,29 @@
-export {};
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+const initialState = {}
+export const productSlice = createSlice({
+        name: 'products',
+        initialState,
+        reducers: {
+            addProduct: (state, action: PayloadAction<any>) => {
+
+            },
+            updateProduct: (state, action: PayloadAction<any>) => {
+
+            },
+            deleteProduct: (state, action: PayloadAction<string>) => {
+
+            },
+            getProducts: (state) => {
+
+            },
+            getProduct: (state, action: PayloadAction<string>) => {
+
+            },
+        }
+    })
+;
+
+export const {addProduct, updateProduct, deleteProduct, getProducts, getProduct} = productSlice.actions;
+export default productSlice.reducer;
+
